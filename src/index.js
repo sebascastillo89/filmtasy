fc19bce532dadd087cdf7a8c8c21ad1e1f073852
Sebastian Castillo Rodriguez <sebastian.castillo@softtek.com>
2020-11-22 14:51:50 +0100

    Polyfill to IE

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
